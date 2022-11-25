acbedc880ebdc72ddf5e72733ebb345d81ea417a
DeNat1ve <deniskutt@yandex.ru>
2022-11-25 12:10:54 +0300

    upload

---
+++ TestApi.Entities/Block.cs
@@ -0,0 +1,16 @@
+﻿namespace TestApi.Entities
+{
+    public class Block
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        public Block(int id, string title, string body) 
+        {
+            Id = id;
+            Title = title;
+            Body = body;
+        }
+    }
+}
---
+++ TestApi/Controllers/BlockController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using TestApi.Entities;
+
+namespace TestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BlockController
+    {
+        [EnableCors("MyPolicy")]
+        [HttpGet]
+        public IEnumerable<Block> Get()
+        {
+            return Enumerable.Range(1, 3).Select(i => new Block(i, "some title", "some body")).ToArray();
+        }
+    }
+}
